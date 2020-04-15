b6a4ea8a22e8807e8e9f1ded0c89d520d5de85a6
Anastasia <lisanastya15@gmail.com>
2020-04-15 16:45:31 +0000

    listen post is changed

--- app.js
+++ app.js
@@ -61,9 +61,9 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, function(){
+// app.listen(3000, function(){
+//   console.log('Camp app is runnning');
+})
+app.listen(process.env.PORT, process.env.IP, function(){
   console.log('Camp app is runnning');
 })
-// app.listen(process.env.PORT, process.env.IP, function(){
-//   console.log('Camp app is runnning');
-// })
