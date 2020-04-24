caee5513defa28ef0023d7a2c0a39e4bed5a846f
Anastasia <lisanastya15@gmail.com>
2020-04-24 11:34:11 +0000

    reseeded

--- app.js
+++ app.js
@@ -62,9 +62,9 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(3000, function(){
-  console.log('Camp app is runnning');
-})
-// app.listen(process.env.PORT, process.env.IP, function(){
+// app.listen(3000, function(){
 //   console.log('Camp app is runnning');
 // })
+app.listen(process.env.PORT, process.env.IP, function(){
+  console.log('Camp app is runnning');
+})
